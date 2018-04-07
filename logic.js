1b923f452890758362a7b5ff1356622fc2a7f6d7
Natalie Chin <chinnh@mcmaster.ca>
2018-04-07 19:40:28 -0400

    method template outlined

---
+++ logic.js
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+/** 
+*Adding a mother object to the system  
+*/ 
+async function addMother(uid) { 
+} 
+
+/** 
+*Adding a mother 
+*/ 
+async function addChild() { 
+} 
+
+/* global getCurrentParticipant getParticipantRegistry getFactory emit */
+
+/**
+ * A Member grants access to their record to another Member.
+ * @param {org.acme.babyregastar.AuthorizeAccess} authorize - the authorize to be processed
+ * @transaction
+ */
